dfeadd7884007da947dd318ac35f246aecf41a8c
rpechayr <romain.pechayre@applidget.com>
2015-07-29 18:59:44 +0200

    Basic json parsing working of github hook

--- server.rb
+++ server.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require "multi_json"
+re
 
-get '/hi' do
-  "Hello World!"
+configure do
+  # logging is enabled by default in classic style applications,
+  # so `enable :logging` is not needed
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
+
+helpers do
+  def parsed_body
+    ::MultiJson.decode(request.body)
+  end
+end
+
+post '/hook' do
+  puts parsed_body
 end
