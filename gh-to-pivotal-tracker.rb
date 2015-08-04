c08ee8918993f0062bef421aecb99f6f5ceeee9d
Gomez Guillaume <guillaume.gomez846@gmail.com>
2015-08-04 17:28:05 +0200

    Move code from Server.rb to gh-to-pivotal-tracker

---
+++ gh-to-pivotal-tracker.rb
@@ -0,0 +1,27 @@
+require "sinatra/base"
+require "sinatra/config_file"
+require "sinatra/reloader"
+require "multi_json"
+
+class GithubToPivotalTracker < Sinatra::Base
+  register Sinatra::ConfigFile
+
+  config_file '#{settings.root}/config/initializers/config.yml'
+
+  set :logging, true
+  set :dump_errors, true
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  helpers do
+  def parsed_body
+    ::MultiJson.decode(request.body)
+  end
+end
+
+  post '/hook' do
+    puts parsed_body
+  end
+end
