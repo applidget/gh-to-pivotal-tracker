7dcd438e5a888809c9660a679cf2bbff5c62f3a9
rpechayr <romain.pechayre@applidget.com>
2015-08-05 17:53:55 +0200

    WIP. ALl pages not loaded yet

--- lib/github_syncer.rb
+++ lib/github_syncer.rb
@@ -1,10 +1,18 @@
 require "octokit"
 
 class GithubSyncer
-  
-  
+
   def run(project)
-    client = Octokit::Client.new(:access_token => APP_CONFIG["github_access_token"])
-    repo = client.
+    
+    current_page = Octokit.issues APP_CONFIG["github_repo_name"], :access_token => APP_CONFIG["github_access_token"]
+    loop do
+      current_page.each do |issue|
+        puts issue["title"]
+      end
+      debugger
+      current_page = Octokit.last_response.rels[:next].get.data
+      break if current_page == nil
+    end
+    nil
   end
 end
