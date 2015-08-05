4e254870636e06c66abdcc0636427921f4042a3d
Gomez Guillaume <guillaume.gomez846@gmail.com>
2015-08-05 18:39:14 +0200

    Add a folder tracked

--- environment.rb
+++ environment.rb
@@ -4,6 +4,6 @@ require "bundler"
 Bundler.require
 
 require "find"
-%w{config/initializers models}.each do |load_path|
+%w{config/initializers models lib}.each do |load_path|
   Find.find(load_path) { |f| require "./#{f}" unless f.match(/\/\..+$/) || File.directory?(f) }
 end
