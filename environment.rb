418c72d26e0a583bebc48399797bc58b720bed1d
Gomez Guillaume <guillaume.gomez846@gmail.com>
2015-08-05 11:18:34 +0200

    Add environment.rb in order to load the config files

---
+++ environment.rb
@@ -0,0 +1,9 @@
+$LOAD_PATH << File.expand_path(File.dirname(__FILE__))
+require "rubygems"
+require "bundler"
+Bundler.require
+
+require "find"
+%w{config/initializers models}.each do |load_path|
+  Find.find(load_path) { |f| require "./#{f}" unless f.match(/\/\..+$/) || File.directory?(f) }
+end
