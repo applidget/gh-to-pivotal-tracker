c5d432620ac51dbb554d27a53352869a8b90102f
Gomez Guillaume <guillaume.gomez846@gmail.com>
2015-08-05 11:25:27 +0200

    add ignore files in the repo

---
+++ config/initializers/mongoid.rb
@@ -0,0 +1,1 @@
+Mongoid.load! File.join(File.dirname(__FILE__), "../mongoid.yml")
---
+++ models/webhook.rb
@@ -0,0 +1,10 @@
+class WebHook
+	include Mongoid::Document
+	include Mongoid::Timestamps
+
+  field :action
+  field :issue, type: Hash
+  field :sender, type: Hash
+
+  validates_presence_of :action, :issue, :sender
+end
