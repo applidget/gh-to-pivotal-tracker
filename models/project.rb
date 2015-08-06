f69cc69efeccad2843c9c94abc870e6e674fb3aa
rpechayr <romain.pechayre@applidget.com>
2015-08-06 17:14:44 +0200

    Remove project model

--- models/project.rb
+++
@@ -1,9 +0,0 @@
-#This model is used to store global sync state
-
-class Project
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  
-  field :last_pt_synced_at, type: DateTime
-  field :list_github_synced_at, type: DateTime
-end
