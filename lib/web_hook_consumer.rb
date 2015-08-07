2153bc132879f8166fd12819d836d19346ba848f
rpechayr <romain.pechayre@applidget.com>
2015-08-07 14:48:11 +0200

    Update syntax error

--- lib/web_hook_consumer.rb
+++ lib/web_hook_consumer.rb
@@ -2,7 +2,7 @@ class WebHookConsumer
   
   def sync
     loop do
-      web_hook = WebHook.where(sync_state: "TODO").find_one_and_update({"$set" => {sync_state: "PROCESSING"}, "$currentDate": {sync_ts: true}})
+      web_hook = WebHook.where(sync_state: "TODO").find_one_and_update({"$set" => {sync_state: "PROCESSING"}, "$currentDate" => {sync_ts: true}})
       break if web_hook.nil?
       consume web_hook
       web_hook.set(sync_state: "DONE", sync_ts: DateTime.now)
