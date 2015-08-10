2976e77ccd96d79a3b18a80ed7001485e13dbb09
Gomez Guillaume <guillaume.gomez846@gmail.com>
2015-08-10 18:22:23 +0200

    change the case statement to a simple if

--- lib/github_updater.rb
+++ lib/github_updater.rb
@@ -2,8 +2,7 @@ class GithubUpdater
 
   def sync
     Ticket.where( gh_state: "open").each do |ticket|
-      case
-      when %w(unscheduled unstarted started).include?(ticket.pivotal_story.current_state)
+      if %w(unscheduled unstarted started).include?(ticket.pivotal_story.current_state)
         ticket.update_github_description
       end
     end
