dbf8eeaf31182ee2c41be10d669b4f7cf97eed65
Gomez Guillaume <guillaume.gomez846@gmail.com>
2015-08-10 18:32:52 +0200

    remove a space

--- lib/github_updater.rb
+++ lib/github_updater.rb
@@ -1,7 +1,7 @@
 class GithubUpdater
 
   def sync
-    Ticket.where( gh_state: "open").each do |ticket|
+    Ticket.where(gh_state: "open").each do |ticket|
       if %w(unscheduled unstarted started).include?(ticket.pivotal_story.current_state)
         ticket.update_github_description
       end
