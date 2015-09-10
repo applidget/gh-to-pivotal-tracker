14fcb5729361881b390611852a14a631b5e62f8d
rpechayr <romain.pechayre@applidget.com>
2015-09-10 09:37:05 +0200

    First easy, but real, test passing

--- spec/models/web_hook_spec.rb
+++ spec/models/web_hook_spec.rb
@@ -2,12 +2,22 @@ require "spec_helper"
 
 describe WebHook do
   before(:each) do
-    @event = WebHook.new
+    issue = {
+      "id" => "12jgfghjk6543",
+      "number" => 7654,
+      "title" => "Some regex issue",
+      "html_url" => "http://github.com",
+      "labels" => [{:name => "bug"}, {name:"qualified"}],
+      "author" => "rpechayr",
+      "state" => "open",
+      "body" => "Some really short body"
+    }
+    @web_hook = WebHook.new issue: issue , action: {key: "value"}, sender: {key: "value"}
   end
 
   describe "#something" do
     it "runs some test" do
-      expect(3+1).to eq 4
+      expect(@web_hook).to be_valid
     end
   end
 end
