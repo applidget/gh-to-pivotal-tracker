bf7306729da6c66420412b71514dab2b7ca4580a
rpechayr <romain.pechayre@applidget.com>
2015-09-22 14:22:10 +0200

    Add gh desc handler class with tests

---
+++ lib/github_description_handler.rb
@@ -0,0 +1,30 @@
+class GithubDescriptionHandler
+  
+  DELIMITER = "--- "
+  
+  #This method appears to block (for how long) for some examples of `from`
+  def self.replace_or_append(from, to_insert, regex)
+    unless from.gsub!(regex, to_insert)
+      from += to_insert
+    end
+    from
+  end
+  
+  def self.github_message(story)
+    message = "\n#{TOKEN}\n"
+    message += "**Pivotal Tracker** - [##{story.id}](#{story.url})\n"
+    message += "*Estimation*: **#{story.estimate} points**\n" if !story.estimate.blank?
+    message += eta_string
+    message += "\n\n#{TOKEN}\n"
+  end
+  
+  def self.eta_string(options)
+    pt_current_eta = options.delete(:current)
+    pt_previous_eta = options.delete(:previous) 
+    display_previous = options.delete(:display_previous) || false 
+    message = "*ETA*: **#{pt_current_eta.strftime("#{pt_current_eta.day.ordinalize} %B %Y")}**" if !pt_current_eta.blank?
+    message += " (was #{pt_previous_eta.strftime("#{pt_previous_eta.day.ordinalize} %B %Y")})" if display_previous && !pt_previous_eta.blank? && pt_previous_eta != pt_current_eta
+    message ||= ""
+  end
+  
+end
---
+++ spec/lib/github_description_handler_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe GithubDescriptionHandler do
+  
+  it "replaces a text when it exists" do
+    text = %{
+      Do someting
+      
+      Do not forget to do smething else. 
+      Process the following TODO list:
+      - [ ] TODO1
+      - [ ] TODO2
+      - [ ] TODO3
+      
+      ---
+    }
+    
+    result = GithubDescriptionHandler.replace_or_append(text, "something", /---/)
+    expected = %{
+      Do someting
+      
+      Do not forget to do smething else. 
+      Process the following TODO list:
+      - [ ] TODO1
+      - [ ] TODO2
+      - [ ] TODO3
+      
+      something
+    }
+    
+    expect(result).to eq(expected)
+  end
+  
+  it "appends a text when it does not exist" do
+    text = %{
+              Do someting
+
+              Do not forget to do smething else. 
+              Process the following TODO list:
+              - [ ] TODO1
+              - [ ] TODO2
+              - [ ] TODO3
+            }
+    
+    result = GithubDescriptionHandler.replace_or_append(text, "something", /---/)
+    expected = %{
+              Do someting
+
+              Do not forget to do smething else. 
+              Process the following TODO list:
+              - [ ] TODO1
+              - [ ] TODO2
+              - [ ] TODO3
+            something}
+    
+    expect(result).to eq(expected)
+  end
+  
+  it "computes eta string based on params", focus: true do
+    eta1 = Date.parse('2015-11-03') 
+    eta2 = Date.parse('2015-11-06')
+    res = GithubDescriptionHandler.eta_string(current: eta1, previous: eta2)
+    expect(res).to eq("*ETA*: **3rd November 2015**")
+    
+    res = GithubDescriptionHandler.eta_string(current: eta1, previous: eta2, display_previous: true)
+    expect(res).to eq("*ETA*: **3rd November 2015** (was 6th November 2015)")
+  end
+  
+  
+end
