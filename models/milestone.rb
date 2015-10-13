7f192d29b4742fd1d5991b5830979edee9ec6e9b
Gomez Guillaume <guillaume.gomez846@gmail.com>
2015-10-13 18:55:42 +0200

    create new model to manage milestone/epic

---
+++ models/milestone.rb
@@ -0,0 +1,60 @@
+class Milestone
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  #Github milestone
+  field :id
+  field :title
+  field :description
+  field :open_issues
+  field :closed_issues
+  field :state
+  field :due_on
+  field :closed_at
+
+  field :pt_epic_id
+
+  def self.create_milestone payload_milestone
+    id = payload_milestone["id"]
+    title = payload_milestone["title"]
+    description = payload_milestone["description"]
+    open_issues = payload_milestone["open_issues"]
+    closed_issues = payload_milestone["closed_issues"]
+    state = payload_milestone["state"]
+    due_on = payload_milestone['due_on']
+    closed_at = payload_milestone["closed_at"]
+
+    milestone = Milestone.insert_or_update(id, title, description, open_issues, closed_at, state, due_on, closed_at)
+    milestone.create_epic
+    milestone
+  end
+
+  def self.insert_or_update(id, title, description, open_issues, closed_issues, state, due_on, closed_at)
+    milestone = Milestone.where(id: id).first
+    params = {
+        title: title,
+        description: description,
+        open_issues: open_issues,
+        closed_issues: closed_issues,
+        state: state,
+        due_on: due_on,
+        closed_at: closed_at,
+      }
+    if milestone.nil?
+      milestone = Milestone.create({id: id}.merge!(params))
+    else
+      milestone.update_attributes(params)
+    end
+    milestone
+  end
+
+  def create_epic
+    return unless pt_epic_id.blank?
+    @pivotal_epic = Ticket.pivotal_project.create_epic(name: title, description: description)
+    if @pivotal_epic.id
+      self.pt_epic_id = @pivotal_epic.id
+      self.save
+    end
+  end
+
+end
