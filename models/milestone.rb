1e25fc7adc6f919d5af1675aa1d763cbb6b3791b
Gomez Guillaume <guillaume.gomez846@gmail.com>
2015-10-15 15:54:33 +0200

    Dynamically load parameters in the payload

--- models/milestone.rb
+++ models/milestone.rb
@@ -15,34 +15,20 @@ class Milestone
 
   field :pt_epic_id
 
-  def self.create_milestone payload_milestone
-    id = payload_milestone["id"]
-    title = payload_milestone["title"]
-    description = payload_milestone["description"]
-    open_issues = payload_milestone["open_issues"]
-    closed_issues = payload_milestone["closed_issues"]
-    state = payload_milestone["state"]
-    due_on = payload_milestone['due_on']
-    closed_at = payload_milestone["closed_at"]
-    html_url = payload_milestone["html_url"]
-
-    milestone = Milestone.insert_or_update(id, title, description, open_issues, closed_at, state, due_on, closed_at, html_url)
+  def self.create_milestone (payload_milestone)
+    milestone = Milestone.insert_or_update(payload_milestone)
     milestone.create_epic
     milestone
   end
 
-  def self.insert_or_update(id, title, description, open_issues, closed_issues, state, due_on, closed_at, html_url)
-    milestone = Milestone.where(id: id).first
-    params = {
-        title: title,
-        description: description,
-        open_issues: open_issues,
-        closed_issues: closed_issues,
-        state: state,
-        due_on: due_on,
-        closed_at: closed_at,
-        html_url: html_url
-      }
+  def self.insert_or_update(payload_milestone)
+    milestone = Milestone.where(id: payload_milestone["id"]).first
+
+    params = Hash.new
+    [:id, :title, :description, :open_issues, :closed_issues, :state, :due_on, :closed_at, :html_url].each do |sym|
+      params[sym] = payload_milestone[sym]
+    end
+
     if milestone.nil?
       milestone = Milestone.create({id: id}.merge!(params))
     else
