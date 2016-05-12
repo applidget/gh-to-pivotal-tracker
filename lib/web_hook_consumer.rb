e603508f1c3dfef1264a482ba8b4bd1c65a65752
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-05-12 15:14:07 +0200

    catch exeption

--- lib/web_hook_consumer.rb
+++ lib/web_hook_consumer.rb
@@ -1,11 +1,18 @@
+require 'byebug'
 class WebHookConsumer
   
   def sync
     loop do
       web_hook = WebHook.where(sync_state: "TODO").find_one_and_update({"$set" => {sync_state: "PROCESSING"}, "$currentDate" => {sync_ts: true}})
       break if web_hook.nil?
-      PayloadLoader.consume(web_hook.issue)
-      web_hook.set(sync_state: "DONE", sync_ts: DateTime.now)
+      begin
+        PayloadLoader.consume(web_hook.issue)
+        web_hook.set(sync_state: "DONE", sync_ts: DateTime.now)
+      rescue Exception => e
+        web_hook.set(sync_state: "TODO")
+        puts "Issue number: '#{web_hook.issue["number"]}', title : '#{web_hook.issue["title"]}' could not be updated in PT"
+         break
+      end
     end
   end
 
